7578e8b140fdde4c20140e26b1fce4561d718267
yusuf200988 <105941838+yusuf200988@users.noreply.github.com>
2023-02-15 19:56:10 +0000

    Delete pages directory

--- pages/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase, SimpleTestCase
-
-
-class SimpleTest(SimpleTestCase):
-    
-    def test_home_page_view(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
